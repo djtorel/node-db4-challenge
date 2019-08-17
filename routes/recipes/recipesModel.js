13f93120a4be21c5f67f845adb08fb9554e4da8f
Dominic Torruellas <dom@domtorr.com>
2019-08-17 11:25:42 -0700

    Added getShoppingList method

--- routes/recipes/recipesModel.js
+++ routes/recipes/recipesModel.js
@@ -2,6 +2,14 @@ const db = require('../../data/dbConfig');
 
 const getRecipes = () => db('recipes');
 
+const getShoppingList = recipe_id =>
+  db('recipes as r')
+    .select('i.ingredient_name', 'ri.quantity')
+    .join('recipes_ingredients as ri', 'r.id', 'ri.recipe_id')
+    .join('ingredients as i', 'i.id', 'ri.ingredient_id')
+    .where({ recipe_id });
+
 module.exports = {
   getRecipes,
+  getShoppingList,
 };
